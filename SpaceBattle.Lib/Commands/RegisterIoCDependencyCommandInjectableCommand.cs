64d37525d33546efff204526f7e28d0630609013
Daniil Tsybulskij <daniil.cybulskij@mail.ru>
2024-12-25 19:50:07 +0600

    Create RegisterIoCDependencyCommandInjectableCommand.cs

---
+++ SpaceBattle.Lib/Commands/RegisterIoCDependencyCommandInjectableCommand.cs
@@ -0,0 +1,14 @@
+﻿namespace SpaceBattle.Lib;
+
+public class RegisterIoCDependencyCommandInjectableCommand : ICommand
+{
+    public void Execute()
+    {
+        IoC.Resolve<Hwdtech.ICommand>(
+                "IoC.Register",
+                "Commands.CommandInjectable",
+                (object[] args) => new CommandInjectableCommand()
+            )
+            .Execute();
+    }
+}
