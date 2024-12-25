e6470dae1b73bc479dffefd16dc0e05e3bc2f3ad
Daniil Tsybulskij <daniil.cybulskij@mail.ru>
2024-12-25 20:59:08 +0600

    Update RegisterIoCDependencyMacroMoveRotateTests.cs

--- SpaceBattle.Tests/RegisterIoCDependencyMacroMoveRotateTests.cs
+++ SpaceBattle.Tests/RegisterIoCDependencyMacroMoveRotateTests.cs
@@ -1,4 +1,4 @@
-using Hwdtech.Ioc;
+﻿using Hwdtech.Ioc;
 using SpaceBattle.Lib;
 
 namespace SpaceBattle.Tests
@@ -8,49 +8,55 @@ namespace SpaceBattle.Tests
         public RegisterIoCDependencyMacroMoveRotateTests()
         {
             new InitScopeBasedIoCImplementationCommand().Execute();
+
+            IoC.Resolve<Hwdtech.ICommand>(
+                    "Scopes.Current.Set",
+                    IoC.Resolve<object>("Scopes.New", IoC.Resolve<object>("Scopes.Root"))
+                )
+                .Execute();
         }
 
         [Fact]
         public void Execute_ShouldRegisterMacroMoveAndRotate()
         {
-            var iocScope = IoC.Resolve<object>("Scopes.New", IoC.Resolve<object>("Scopes.Root"));
-            IoC.Resolve<ICommand>("Scopes.Current.Set", iocScope).Execute();
-
             var moveSpec = new[] { "MoveCommand1", "MoveCommand2" };
             var rotateSpec = new[] { "RotateCommand1", "RotateCommand2" };
 
             var moveCommandMocks = moveSpec.Select(cmd =>
             {
                 var mock = new Mock<ICommand>();
-                IoC.Resolve<ICommand>("IoC.Register", cmd, (object[] args) => mock.Object).Execute();
+                IoC.Resolve<Hwdtech.ICommand>("IoC.Register", cmd, (object[] args) => mock.Object).Execute();
                 return mock;
             }).ToArray();
 
             var rotateCommandMocks = rotateSpec.Select(cmd =>
             {
                 var mock = new Mock<ICommand>();
-                IoC.Resolve<ICommand>("IoC.Register", cmd, (object[] args) => mock.Object).Execute();
+                IoC.Resolve<Hwdtech.ICommand>("IoC.Register", cmd, (object[] args) => mock.Object).Execute();
                 return mock;
             }).ToArray();
 
-            IoC.Resolve<ICommand>("IoC.Register", "Specs.Move", (object[] args) => moveSpec).Execute();
-            IoC.Resolve<ICommand>("IoC.Register", "Specs.Rotate", (object[] args) => rotateSpec).Execute();
+            IoC.Resolve<Hwdtech.ICommand>("IoC.Register", "Specs.Move", (object[] args) => moveSpec).Execute();
+            IoC.Resolve<Hwdtech.ICommand>("IoC.Register", "Specs.Rotate", (object[] args) => rotateSpec).Execute();
 
             new RegisterIoCDependencyMacroMoveRotate().Execute();
 
-            var moveMacro = IoC.Resolve<ICommand>("Macro.Move");
-            moveMacro.Execute();
-            var rotateMacro = IoC.Resolve<ICommand>("Macro.Rotate");
-            rotateMacro.Execute();
+            var moveMacro = IoC.Resolve<ICommand>("Macro.Move", new object[] { "arg1", "arg2" });
+            var rotateMacro = IoC.Resolve<ICommand>("Macro.Rotate", new object[] { "arg1", "arg2" });
 
+            Assert.IsType<MacroCommand>(moveMacro);
+            Assert.IsType<MacroCommand>(rotateMacro);
+
+            moveMacro.Execute();
             foreach (var mock in moveCommandMocks)
             {
-                mock.Verify(cmd => cmd.Execute(), Times.Once()); 
+                mock.Verify(cmd => cmd.Execute(), Times.Once());
             }
 
+            rotateMacro.Execute();
             foreach (var mock in rotateCommandMocks)
             {
-                mock.Verify(cmd => cmd.Execute(), Times.Once()); 
+                mock.Verify(cmd => cmd.Execute(), Times.Once());
             }
         }
     }
