7da1ec74b7602b470fe9bbf0382d94cd200bd240
Denis0987 <denis8.9@bk.ru>
2025-05-13 23:42:50 +0600

    auth

---
+++ SpaceBattle.Lib/Commands/RegisterIoCDependencyAuthCheck.cs
@@ -0,0 +1,38 @@
+﻿namespace SpaceBattle.Lib.Commands
+{
+    public class RegisterIoCDependencyAuthCheck : ICommand
+    {
+        public void Execute()
+        {
+            IoC.Resolve<ICommand>(
+                "IoC.Register",
+                "Authorization.Check",
+                new Func<object[], object>(AuthResolver)
+            ).Execute();
+        }
+
+        private object AuthResolver(object[] parameters)
+        {
+            var userId = (string)parameters[0];
+            var action = (string)parameters[1];
+            var resourceId = (string)parameters[2];
+
+            var perms = IoC.Resolve<IDictionary<string, IEnumerable<string>>>(
+                "Authorization.GetPermissions",
+                userId
+            );
+
+            if (perms.TryGetValue("*", out _))
+            {
+                return (object)true;
+            }
+
+            if (!perms.TryGetValue(resourceId, out var objPerms))
+            {
+                return (object)false;
+            }
+
+            return (object)(objPerms.Contains("*") || objPerms.Contains(action));
+        }
+    }
+}
