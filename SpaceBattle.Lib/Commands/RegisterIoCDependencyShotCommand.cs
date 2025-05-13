c1fbc710c95b13eec6a5ca69ee9cda4667b227d2
Denis0987 <145117807+Kuzina-Varya@users.noreply.github.com>
2025-05-13 23:15:15 +0600

    Команда выстрела и движения

---
+++ SpaceBattle.Lib/Commands/RegisterIoCDependencyShotCommand.cs
@@ -0,0 +1,21 @@
+﻿using Hwdtech.Ioc;
+using SpaceBattle.Lib.Interfaces;
+
+namespace SpaceBattle.Lib.Commands
+{
+    public class RegisterIoCDependencyShotCommand : ICommand
+    {
+        public void Execute()
+        {
+            IoC.Resolve<ICommand>(
+                "IoC.Register",
+                "Commands.Shot",
+                (object[] parameters) =>
+                {
+                    var shooter = (IShooting)parameters[0];
+                    return new ShotCommand(shooter);
+                }
+            ).Execute();
+        }
+    }
+}
---
+++ SpaceBattle.Lib/Commands/ShotCommand.cs
@@ -0,0 +1,33 @@
+﻿using SpaceBattle.Lib.Interfaces;
+
+namespace SpaceBattle.Lib.Commands
+{
+    public class ShotCommand : ICommand
+    {
+        private readonly IShooting _shooter;
+
+        public ShotCommand(IShooting shooter)
+        {
+            _shooter = shooter;
+        }
+
+        public void Execute()
+        {
+            var projectile = IoC.Resolve<IShooting>("Game.Projectile.Create");
+
+            var initCmd = IoC.Resolve<ICommand>(
+                "Game.Commands.InitializeProjectile",
+                projectile,
+                _shooter
+            );
+            initCmd.Execute();
+
+            var moveCmd = IoC.Resolve<ICommand>(
+                "Actions.Start",
+                projectile,
+                "Move"
+            );
+            moveCmd.Execute();
+        }
+    }
+}
