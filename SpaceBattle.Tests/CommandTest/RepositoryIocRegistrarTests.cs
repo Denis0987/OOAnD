f6300bfb38515a1d7644103c35440a24c17e1bd7
Denis0987 <33244014+Denis0987@users.noreply.github.com>
2025-05-27 22:39:52 +0600

    test(repository): add tests for argument validation in Repository commands

--- SpaceBattle.Tests/CommandTest/RepositoryIocRegistrarTests.cs
+++ SpaceBattle.Tests/CommandTest/RepositoryIocRegistrarTests.cs
@@ -1,29 +1,90 @@
-﻿using SpaceBattle.Lib.Commands;
+using Hwdtech;
+using SpaceBattle.Lib.Commands;
 
 namespace SpaceBattle.Tests.CommandTest
 {
     public class RepositoryIocRegistrarTests
     {
+        public RepositoryIocRegistrarTests()
+        {
+            var root = IoC.Resolve<object>("Scopes.Root");
+            var scope = IoC.Resolve<object>("Scopes.New", root);
+            IoC.Resolve<ICommand>("Scopes.Current.Set", scope).Execute();
+        }
+
+
         [Fact]
-        public void Execute_RegistersAddRemoveFetchFactories()
+        public void AddCommand_Throws_OnDuplicateUid()
         {
             var registrar = new RepositoryIocRegistrar();
-
             registrar.Execute();
+            var uid = Guid.NewGuid().ToString();
+            var entry1 = new Dictionary<string, object> { ["name"] = "E1", ["uid"] = uid };
+            var entry2 = new Dictionary<string, object> { ["name"] = "E2", ["uid"] = uid };
+            var addCmd1 = IoC.Resolve<ICommand>("Repository.Add", new object[] { entry1 });
+            addCmd1.Execute();
+            var addCmd2 = IoC.Resolve<ICommand>("Repository.Add", new object[] { entry2 });
+            Assert.Throws<InvalidOperationException>(() => addCmd2.Execute());
+        }
 
-            var addEntry = new Dictionary<string, object> { ["name"] = "E1", ["uid"] = Guid.NewGuid() };
-            var addCmd = IoC.Resolve<ICommand>("Repository.Add", new object[] { addEntry });
-            Assert.IsType<RepositoryAddCommand>(addCmd);
-            addCmd.Execute();
+        [Fact]
+        public void RemoveCommand_Throws_OnMissingUid()
+        {
+            var registrar = new RepositoryIocRegistrar();
+            registrar.Execute();
+            var uid = Guid.NewGuid().ToString();
+            var removeCmd = IoC.Resolve<ICommand>("Repository.Remove", new object[] { uid });
+            Assert.Throws<KeyNotFoundException>(() => removeCmd.Execute());
+        }
 
-            var removeCmd = IoC.Resolve<ICommand>("Repository.Remove", new object[] { addEntry["uid"]! });
-            Assert.IsType<RepositoryRemoveCommand>(removeCmd);
 
-            var fetched = IoC.Resolve<IDictionary<string, object>>(
-                "Repository.Fetch",
-                new object[] { addEntry["uid"]! }
-            );
-            Assert.Same(addEntry, fetched);
+        [Fact]
+        public void AddCommand_Throws_OnNullEntry()
+        {
+            var registrar = new RepositoryIocRegistrar();
+            registrar.Execute();
+            Assert.Throws<ArgumentException>(() => IoC.Resolve<ICommand>("Repository.Add", new object[] { null! }));
+        }
+
+        [Fact]
+        public void IoC_Fetch_Throws_OnUnknownUid()
+        {
+            var registrar = new RepositoryIocRegistrar();
+            registrar.Execute();
+            var uid = Guid.NewGuid().ToString();
+            Assert.Throws<KeyNotFoundException>(() => IoC.Resolve<IDictionary<string, object>>("Repository.Fetch", new object[] { uid }));
+        }
+
+        [Fact]
+        public void FetchCommand_Throws_OnEmptyUid()
+        {
+            var registrar = new RepositoryIocRegistrar();
+            registrar.Execute();
+            Assert.Throws<ArgumentException>(() => IoC.Resolve<IDictionary<string, object>>("Repository.Fetch", new object[] { "   " }));
+        }
+
+        [Fact]
+        public void AddCommand_Throws_OnIncorrectType()
+        {
+            var registrar = new RepositoryIocRegistrar();
+            registrar.Execute();
+            Assert.Throws<ArgumentException>(() => IoC.Resolve<ICommand>("Repository.Add", new object[] { 123 }));
+        }
+
+        [Fact]
+        public void RemoveCommand_Throws_OnNullUid()
+        {
+            var registrar = new RepositoryIocRegistrar();
+            registrar.Execute();
+            Assert.Throws<ArgumentException>(() => IoC.Resolve<ICommand>("Repository.Remove", new object[] { null! }));
+        }
+
+        [Fact]
+        public void FetchCommand_Throws_OnNullUid()
+        {
+            var registrar = new RepositoryIocRegistrar();
+            registrar.Execute();
+            Assert.Throws<ArgumentException>(() => IoC.Resolve<IDictionary<string, object>>("Repository.Fetch", new object[] { null! }));
         }
     }
 }
