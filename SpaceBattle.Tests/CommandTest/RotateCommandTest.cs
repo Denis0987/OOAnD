e284b89a0140bc3ec722741c58e97f1095b1418a
Denis0987 <33244014+Denis0987@users.noreply.github.com>
2025-05-28 11:38:09 +0600

    Update RotateCommandTest.cs

--- SpaceBattle.Tests/CommandTest/RotateCommandTest.cs
+++ SpaceBattle.Tests/CommandTest/RotateCommandTest.cs
@@ -1,4 +1,5 @@
 ﻿using SpaceBattle.Lib;
+
 namespace SpaceBattle.Tests;
 
 public class RotateCommandTest
@@ -43,5 +44,87 @@ public class RotateCommandTest
         rotatingObj.SetupGet(x => x.RotateVelocity).Returns(new Angle(657, 8));
         var rotateCommand = new RotateCommand(rotatingObj.Object);
         Assert.Throws<Exception>(() => rotateCommand.Execute());
+    }
+
+    [Fact]
+    public void Rotate_WithAngleNormalization_WrapsAroundCorrectly()
+    {
+        // Arrange
+        var rotatingObj = new Mock<IRotating>();
+        rotatingObj.SetupGet(x => x.AnglePosition).Returns(new Angle(7, 8));  // 315 degrees
+        rotatingObj.SetupGet(x => x.RotateVelocity).Returns(new Angle(1, 8));  // 45 degrees
+        var rotateCommand = new RotateCommand(rotatingObj.Object);
+
+        // Act
+        rotateCommand.Execute();
+
+        // Assert - Should wrap around to 0/8 (0 degrees)
+        rotatingObj.VerifySet(x => x.AnglePosition = new Angle(0, 8));
+    }
+
+    [Fact]
+    public void Rotate_WithNegativeAnglePosition_HandlesCorrectly()
+    {
+        // Arrange - This tests the Angle constructor's handling of negative values
+        var rotatingObj = new Mock<IRotating>();
+        rotatingObj.SetupGet(x => x.AnglePosition).Returns(new Angle(-1, 8));  // Should normalize to 7/8 (315 degrees)
+        rotatingObj.SetupGet(x => x.RotateVelocity).Returns(new Angle(1, 8));   // 45 degrees
+        var rotateCommand = new RotateCommand(rotatingObj.Object);
+
+        // Act
+        rotateCommand.Execute();
+
+        // Assert - -1/8 + 1/8 = 0/8 (0 degrees)
+        rotatingObj.VerifySet(x => x.AnglePosition = new Angle(0, 8));
+    }
+
+    [Fact]
+    public void Rotate_WithZeroVelocity_DoesNotChangeAngle()
+    {
+        // Arrange
+        var rotatingObj = new Mock<IRotating>();
+        var initialAngle = new Angle(3, 8);  // 135 degrees
+        rotatingObj.SetupGet(x => x.AnglePosition).Returns(initialAngle);
+        rotatingObj.SetupGet(x => x.RotateVelocity).Returns(new Angle(0, 8));  // 0 degrees
+        var rotateCommand = new RotateCommand(rotatingObj.Object);
+
+        // Act
+        rotateCommand.Execute();
+
+        // Assert - Angle should remain unchanged
+        rotatingObj.VerifySet(x => x.AnglePosition = initialAngle);
+    }
+
+    [Fact]
+    public void Rotate_WithLargeVelocity_HandlesMultipleRotations()
+    {
+        // Arrange
+        var rotatingObj = new Mock<IRotating>();
+        rotatingObj.SetupGet(x => x.AnglePosition).Returns(new Angle(0, 8));
+        // 2 full rotations + 90 degrees
+        rotatingObj.SetupGet(x => x.RotateVelocity).Returns(new Angle(16 + 2, 8));
+        var rotateCommand = new RotateCommand(rotatingObj.Object);
+
+        // Act
+        rotateCommand.Execute();
+
+        // Assert - Should be at 90 degrees (2/8)
+        rotatingObj.VerifySet(x => x.AnglePosition = new Angle(2, 8));
+    }
+
+    [Fact]
+    public void Rotate_WithMaxAngleValue_HandlesCorrectly()
+    {
+        // Arrange
+        var rotatingObj = new Mock<IRotating>();
+        rotatingObj.SetupGet(x => x.AnglePosition).Returns(new Angle(7, 8));  // 315 degrees
+        rotatingObj.SetupGet(x => x.RotateVelocity).Returns(new Angle(1, 8));  // 45 degrees
+        var rotateCommand = new RotateCommand(rotatingObj.Object);
+
+        // Act
+        rotateCommand.Execute();
+
+        // Assert - Should wrap around to 0/8 (0 degrees)
+        rotatingObj.VerifySet(x => x.AnglePosition = new Angle(0, 8));
     }
 }
