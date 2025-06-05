4790c7f4a61b3d80ff89aade2f9534e76516b9e3
Denis0987 <33244014+Denis0987@users.noreply.github.com>
2025-06-06 05:29:17 +0600

    Update CollisionDataWriterCommand.cs

--- SpaceBattle.Lib/Commands/CollisionDataWriterCommand.cs
+++ SpaceBattle.Lib/Commands/CollisionDataWriterCommand.cs
@@ -1,23 +1,33 @@
 ﻿namespace SpaceBattle.Lib.Commands;
 
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using Hwdtech;
 
 public class CollisionDataWriterCommand : ICommand
 {
     private readonly string _fileName;
     private readonly IList<int[]> _collisionPoints;
+    private readonly IFileSystem _fileSystem;
+    private readonly IStorageDirectoryProvider _directoryProvider;
 
-    public CollisionDataWriterCommand(string fileName, IList<int[]> collisionPoints)
+    public CollisionDataWriterCommand(
+        string fileName,
+        IList<int[]> collisionPoints,
+        IFileSystem fileSystem = null,
+        IStorageDirectoryProvider directoryProvider = null)
     {
         _fileName = fileName ?? throw new ArgumentNullException(nameof(fileName));
         _collisionPoints = collisionPoints ?? throw new ArgumentNullException(nameof(collisionPoints));
+        _fileSystem = fileSystem ?? new DefaultFileSystem();
+        _directoryProvider = directoryProvider ?? new DefaultStorageDirectoryProvider();
     }
 
     public void Execute()
     {
-        var storageDir = IoC.Resolve<string>("Collision.StorageDirectory");
+        var storageDir = _directoryProvider.GetStorageDirectory();
 
         if (string.IsNullOrEmpty(storageDir))
         {
@@ -32,12 +42,12 @@ public class CollisionDataWriterCommand : ICommand
                 .ToList();
 
             var dir = Path.GetDirectoryName(fullPath);
-            if (!string.IsNullOrEmpty(dir) && !Directory.Exists(dir))
+            if (!string.IsNullOrEmpty(dir) && !_fileSystem.DirectoryExists(dir))
             {
-                Directory.CreateDirectory(dir);
+                _fileSystem.CreateDirectory(dir);
             }
 
-            File.WriteAllLines(fullPath, lines);
+            _fileSystem.WriteAllLines(fullPath, lines);
         }
         catch (Exception ex) when (ex is ArgumentException || ex is NotSupportedException || ex is UnauthorizedAccessException || ex is PathTooLongException)
         {
@@ -51,5 +61,29 @@ public class CollisionDataWriterCommand : ICommand
         {
             throw new InvalidOperationException("Error writing to file", ex);
         }
+    }
+
+    public interface IFileSystem
+    {
+        void CreateDirectory(string path);
+        void WriteAllLines(string path, IEnumerable<string> contents);
+        bool DirectoryExists(string path);
+    }
+
+    public interface IStorageDirectoryProvider
+    {
+        string GetStorageDirectory();
+    }
+
+    private class DefaultFileSystem : IFileSystem
+    {
+        public void CreateDirectory(string path) => Directory.CreateDirectory(path);
+        public void WriteAllLines(string path, IEnumerable<string> contents) => File.WriteAllLines(path, contents);
+        public bool DirectoryExists(string path) => Directory.Exists(path);
+    }
+
+    private class DefaultStorageDirectoryProvider : IStorageDirectoryProvider
+    {
+        public string GetStorageDirectory() => IoC.Resolve<string>("Collision.StorageDirectory");
     }
 }
